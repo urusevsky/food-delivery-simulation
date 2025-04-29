739d63478e30e5521a2f2530cd020c0e50bf9fae
Wayne Tseng <a10010321@gmail.com>
2025-04-29 20:34:53 +0800

    Create location utility module

---
+++ delivery_sim/utils/location_utils.py
@@ -0,0 +1,25 @@
+def calculate_distance(loc1, loc2):
+    """
+    Calculate Euclidean distance between two locations.
+    
+    Args:
+        loc1: First location [x, y]
+        loc2: Second location [x, y]
+        
+    Returns:
+        float: Euclidean distance between locations
+    """
+    return ((loc1[0] - loc2[0])**2 + (loc1[1] - loc2[1])**2)**0.5
+
+def locations_are_equal(loc1, loc2):
+    """
+    Check if two locations are the same.
+    
+    Args:
+        loc1: First location [x, y]
+        loc2: Second location [x, y]
+        
+    Returns:
+        bool: True if locations are equal, False otherwise
+    """
+    return loc1[0] == loc2[0] and loc1[1] == loc2[1]
